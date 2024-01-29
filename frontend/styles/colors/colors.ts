1b45e2935b66bff2336a6e3132344e20403a22ac
DevLJW <jaewookv123@gmail.com>
2024-01-29 22:27:11 +0900

    colors add

---
+++ frontend/styles/colors/colors.ts
@@ -0,0 +1,16 @@
+export const Common = {
+  colors: {
+    mainColor01: "#323232",
+    mainColor02: "#37334D",
+    mainColor03: "#392F6C",
+    mainColor04: "#62569F",
+    mainColor05: "#9988BD",
+    mainColor06: "#B8A2CF",
+
+    lightGray01: "#D9D9D9",
+
+    white01: "#ffffff",
+
+    black01: "#000000",
+  },
+};
