a8a264bd988546cdbdbf14b5daf43c1e09427d0e
DevLJW <jaewookv123@gmail.com>
2024-02-06 19:49:14 +0900

    FreeBoard index add

---
+++ frontend/pages/FreeBoard/index.tsx
@@ -0,0 +1,5 @@
+import BoardListPage from "../../src/components/units/board/list/BoardList.container";
+
+export default function freeboardPage() {
+  return <BoardListPage isBoolean={true}></BoardListPage>;
+}
