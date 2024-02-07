580c0b699dc5c3f401a3777f7c6990626b5e3d03
DevLJW <jaewookv123@gmail.com>
2024-02-07 21:47:13 +0900

    LayoutHeader type add

---
+++ frontend/src/commons/layout/header/LayoutHeader.types.ts
@@ -0,0 +1,14 @@
+import { ILayoutProps } from "..";
+import { IQuery } from "../../types/generated/types";
+
+export interface ILayoutHeaderUI {
+  onClickLogo: () => void;
+  onClickMoveToLogin: () => void;
+
+  onClickMoveToUserJoin: () => void;
+  onClickMoveToShoppingBaskets: () => void;
+  onClickMoveToFreeBoard: () => void;
+  onClickMoveToMarket: () => void;
+  onClickMoveToannouncement: () => void;
+  UserInfoData: Pick<IQuery, "fetchUserLoggedIn"> | undefined;
+}
