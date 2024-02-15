ea8305fbb2d45b56ee98d225efd116cd20b5eb06
DevLJW <jaewookv123@gmail.com>
2024-02-15 22:27:03 +0900

    FormItem Style add

---
+++ frontend/src/components/CommonComponents/FormItem/FormItem.styles.ts
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+import React from "react";
+
+export const FormItem = styled.div`
+  padding-top: 15px;
+  padding-bottom: 20px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
