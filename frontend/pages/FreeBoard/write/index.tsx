2a0bf77f3f7e6479786f8b4ae2b2900559f5561e
DevLJW <jaewookv123@gmail.com>
2024-02-17 23:32:30 +0900

    Write index add

---
+++ frontend/pages/FreeBoard/write/index.tsx
@@ -0,0 +1,6 @@
+import FreeBoardWrite from "../../../src/components/units/NoticeBoard/FreeBoard/Write/FreeBoardWrite.container";
+
+//  등록페이지
+export default function BoardWritePage() {
+  return <FreeBoardWrite></FreeBoardWrite>;
+}
