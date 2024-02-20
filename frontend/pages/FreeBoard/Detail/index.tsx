3cc591686182e0c20dca05e05c4067847dea604b
DevLJW <jaewookv123@gmail.com>
2024-02-20 22:42:27 +0900

    frontend FreeBoard Detail index add

---
+++ frontend/pages/FreeBoard/Detail/index.tsx
@@ -0,0 +1,5 @@
+import FreeBoardDetail from "../../../src/components/units/NoticeBoard/FreeBoard/Detail/FreeBoardDetail.container";
+
+export default function BoardDetailPage() {
+  return <FreeBoardDetail></FreeBoardDetail>;
+}
