b742488428de23022064d523e4a4cce8177899f2
DevLJW <jaewookv123@gmail.com>
2024-02-20 22:41:28 +0900

    frontend list index add

---
+++ frontend/pages/FreeBoard/list/index.tsx
@@ -0,0 +1,5 @@
+import FreeBoardList from "../../../src/components/units/NoticeBoard/FreeBoard/List/FreeBoardList.container";
+
+export default function freeboardPage() {
+  return <FreeBoardList></FreeBoardList>;
+}
