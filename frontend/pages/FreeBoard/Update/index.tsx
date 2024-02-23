bc9c4bdfa930c350836aaabaaaae3014b0ca77ac
DevLJW <jaewookv123@gmail.com>
2024-02-23 23:43:58 +0900

    update index add

---
+++ frontend/pages/FreeBoard/Update/index.tsx
@@ -0,0 +1,5 @@
+import FreeBoardUpdate from "../../../src/components/units/NoticeBoard/FreeBoard/Update/FreeBoardUpdate.container";
+
+export default function UpdatePage() {
+  return <FreeBoardUpdate></FreeBoardUpdate>;
+}
