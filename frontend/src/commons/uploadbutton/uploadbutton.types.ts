86f0eeef2b92b5ec409d99df2e0bc5baac572b6c
DevLJW <jaewookv123@gmail.com>
2024-02-26 22:36:21 +0900

    upload button type add

---
+++ frontend/src/commons/uploadbutton/uploadbutton.types.ts
@@ -0,0 +1,14 @@
+import { ChangeEvent, RefObject } from "react";
+
+export interface UploadButtonProps {
+  index: number;
+  onChangeFileUrls: (FileUrl: string, index: number) => void;
+  FileUrl: string;
+}
+
+export interface UploadButtonUIProps {
+  fileRef?: RefObject<HTMLInputElement>;
+  UploadButtonClick?: () => void;
+  onChangeFile?: (event: ChangeEvent<HTMLInputElement>) => Promise<void>;
+  FileUrl?: string;
+}
