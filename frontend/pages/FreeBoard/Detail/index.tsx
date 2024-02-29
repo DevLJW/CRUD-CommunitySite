a7458b893d25e9c48a128cc616648cc25a9ebac7
DevLJW <jaewookv123@gmail.com>
2024-02-29 23:40:45 +0900

    board Detail page update

--- frontend/pages/FreeBoard/Detail/index.tsx
+++ frontend/pages/FreeBoard/Detail/index.tsx
@@ -1,5 +1,13 @@
+import FreeBoardCommentList from "../../../src/components/units/NoticeBoard/FreeBoard/Comment/List/FreeBoardCommentList.container";
+import FreeBoardComment from "../../../src/components/units/NoticeBoard/FreeBoard/Comment/Write/FreeBoardComment.container";
 import FreeBoardDetail from "../../../src/components/units/NoticeBoard/FreeBoard/Detail/FreeBoardDetail.container";
 
 export default function BoardDetailPage() {
-  return <FreeBoardDetail></FreeBoardDetail>;
+  return (
+    <>
+      <FreeBoardDetail></FreeBoardDetail>
+      <FreeBoardComment></FreeBoardComment>
+      <FreeBoardCommentList></FreeBoardCommentList>
+    </>
+  );
 }
