5061f8b70c75adb74025718c5ef8e25bac76ffcc
DevLJW <jaewookv123@gmail.com>
2024-04-11 23:54:42 +0900

    backend createboard input add

---
+++ backend/src/APIS/FreeBoards/dto/create-board.input.ts
@@ -0,0 +1,15 @@
+import { Field, InputType } from "@nestjs/graphql";
+
+@InputType() //  데이터가 들어오는 타입은 Inputtype으로 Graphql 명세에 표기해야 한다.
+export class CreateFreeBoardInput {
+  @Field(() => String)
+  writer: string;
+  @Field(() => String)
+  password: string;
+  @Field(() => String)
+  title: string;
+  @Field(() => String)
+  content: string;
+  @Field(() => [String], { nullable: true })
+  FileUrls?: string[];
+}
