83daf2d4ff817ad55965cffb3086d61dac12f278
DevLJW <jaewookv123@gmail.com>
2024-05-07 11:38:03 +0900

    Login index add

--- frontend/pages/Login/index.tsx
+++ frontend/pages/Login/index.tsx
@@ -1,4 +1,3 @@
-import JoinLogin from "../../src/components/units/User/Join_Login/Join_Login.container";
 import Login from "../../src/components/units/User/Login/Login.container";
 
 export default function LoginPage() {
