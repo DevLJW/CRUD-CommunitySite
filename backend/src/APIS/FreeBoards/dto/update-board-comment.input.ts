f622dc3d3667a3d9831639a7392964d47e19c20e
DevLJW <jaewookv123@gmail.com>
2024-05-18 23:34:27 +0900

    Update Board Comment Input add

---
+++ backend/src/APIS/FreeBoards/dto/update-board-comment.input.ts
@@ -0,0 +1,7 @@
+import { Field, InputType } from "@nestjs/graphql";
+
+@InputType() //  데이터가 들어오는 타입은 Inputtype으로 Graphql 명세에 표기해야 한다.
+export class UpdateFreeBoardCommentInput {
+  @Field(() => String)
+  content: string;
+}
