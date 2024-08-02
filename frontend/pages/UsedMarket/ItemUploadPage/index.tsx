c032a34d454125ecb9e6bcd8c9520788245f4499
DevLJW <jaewookv123@gmail.com>
2024-08-02 18:51:35 +0900

    UsedMarket file add

---
+++ frontend/pages/UsedMarket/ItemUploadPage/index.tsx
@@ -0,0 +1,5 @@
+// import UsedMarketItemUploadUI from "../../../src/components/units/UsedMarket/Upload_Edit/UsedMarketItemUpload_Edit.presenter";
+
+// export default function ItemUploadPage() {
+//   return <UsedMarketItemUploadUI boolean={true}></UsedMarketItemUploadUI>;
+// }
