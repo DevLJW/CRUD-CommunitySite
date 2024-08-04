b36939e21b9b2607b005e3df6ebc65c9b450c18c
DevLJW <jaewookv123@gmail.com>
2024-08-05 03:31:33 +0900

    formcontainer file update

--- frontend/src/components/CommonComponents/Form/Form.container.tsx
+++ frontend/src/components/CommonComponents/Form/Form.container.tsx
@@ -2,5 +2,10 @@ import { IForm } from "./Form.interfaces";
 import FormUI from "./Form.presenter";
 
 export default function Form(props: IForm) {
-  return <FormUI onSubmit={props.onSubmit}> {props.children}</FormUI>;
+  return (
+    <FormUI onSubmit={props.onSubmit} style={props.style}>
+      {" "}
+      {props.children}
+    </FormUI>
+  );
 }
